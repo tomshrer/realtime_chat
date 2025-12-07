0b90337d78de7b9e7731f435e9dff975f79eaf93
Tomshr29 <tomscherer29@gmail.com>
2025-12-07 15:41:01 +0100

    chore: setup TailwindCSS

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,15 @@ import { getDirname } from '@adonisjs/core/helpers'
 import inertia from '@adonisjs/inertia/client'
 import react from '@vitejs/plugin-react'
 import adonisjs from '@adonisjs/vite/client'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [inertia({ ssr: { enabled: true, entrypoint: 'inertia/app/ssr.tsx' } }), react(), adonisjs({ entrypoints: ['inertia/app/app.tsx'], reload: ['resources/views/**/*.edge'] })],
+  plugins: [
+    inertia({ ssr: { enabled: true, entrypoint: 'inertia/app/ssr.tsx' } }),
+    react(),
+    adonisjs({ entrypoints: ['inertia/app/app.tsx'], reload: ['resources/views/**/*.edge'] }),
+    tailwindcss(),
+  ],
 
   /**
    * Define aliases for importing modules from
